77476f3c190a5bdb93fd77498870226d97514a85
pengliang <3495268188@qq.com>
2022-08-29 11:27:04 +0800

    完成PermissionX库的开发与测试工作。

--- app/src/main/java/com/liang/permissionx/MainActivity.kt
+++ app/src/main/java/com/liang/permissionx/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.liang.permissionx
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.liang.permissionx.databinding.ActivityMainBinding
+import com.permissionx.pengliangdev.PermissionX
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.makeCallBtn.setOnClickListener {
+            PermissionX.request(this, Manifest.permission.CALL_PHONE){
+                allGranted,deniedList->
+                if (allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this,"You denied $deniedList",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        try {
+        val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
